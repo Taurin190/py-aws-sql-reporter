a22f7238897b8f9f368d4e783752702a7ee2e87d
Koichi Taura <taura.koichi@gmail.com>
2020-02-21 00:07:06 +0900

    divide static and class method which use tmp directory

--- store/store_file.py
+++ store/store_file.py
@@ -1,12 +1,23 @@
 
 class StoreFile:
 
+    def __init__(self, config=None):
+        self.tmp_path = "./tmp"
+        if config and "tmp_path" in config.keys():
+            self.tmp_path = config["tmp_path"]
+
+    def store_file_on_tmp(self, file_name, text):
+        return StoreFile.store(self.tmp_path + '/' + file_name, text)
+
+    def get_file_from_tmp(self, file_path):
+        return StoreFile.get_file(self.tmp_path + '/' + file_path)
+
     @staticmethod
-    def store(file_name, text):
-        with open('./tmp/' + file_name, "w") as f:
+    def store(file_path, text):
+        with open(file_path, "w") as f:
             f.write(text)
 
     @staticmethod
-    def get_file(file_name):
-        with open('./tmp/' + file_name, "r") as f:
+    def get_file(file_path):
+        with open(file_path, "r") as f:
             return f.read()
