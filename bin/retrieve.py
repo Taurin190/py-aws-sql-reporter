889af925d3fc768ec9743daa46fd225381270d7f
Koichi Taura <taura.koichi@gmail.com>
2020-02-21 00:06:03 +0900

    implement sql_handler on retrieve file

--- bin/retrieve.py
+++ bin/retrieve.py
@@ -1,15 +1,22 @@
 from sql.sql_handler import SQLHandler
+from database.mysql import MySQL
 
 
 class Retriever:
     def __init__(self, config=None):
         self.sql_handler = SQLHandler(config)
+        self.db = MySQL(config)
 
     def get_all(self):
         files = self.sql_handler.get_all_files()
         for file in files:
             self.get(file)
 
-    def get(self, sql):
-        if self.sql_handler.exist_sql_file(sql):
-            print(sql)
+    def get(self, sql_file):
+        if not self.sql_handler.exist_sql_file(sql_file):
+            exit(1)
+        sql = self.sql_handler.get_sql_contents(sql_file)
+        print(sql)
+        query_result = self.db.get_from_sql(sql)
+        print(query_result)
+
--- sql/sql_handler.py
+++ sql/sql_handler.py
@@ -14,3 +14,10 @@ class SQLHandler:
 
     def exist_sql_file(self, file_path):
         return os.path.exists(self.sql_path + "/" + file_path)
+
+    def get_sql_contents(self, sql_path):
+        sql_contents = ""
+        if self.exist_sql_file(sql_path):
+            with open(sql_contents) as f:
+                sql_contents = f.read()
+        return sql_contents
