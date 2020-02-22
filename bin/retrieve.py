7147a54935dfd5e38b7aa55b3c657befd73cdf27
Koichi Taura <taura.koichi@gmail.com>
2020-02-22 10:43:16 +0900

    add fill excel feature

--- bin/retrieve.py
+++ bin/retrieve.py
@@ -1,11 +1,24 @@
+import os
+import datetime
 from sql.sql_handler import SQLHandler
 from database.mysql import MySQL
+from store.store_file import StoreFile
+from store.store_excel import StoreExcel
 
 
 class Retriever:
     def __init__(self, config=None):
         self.sql_handler = SQLHandler(config)
         self.db = MySQL(config['database'])
+        dt = datetime.datetime.now()
+        self.directory_name = dt.strftime('%Y%m%d%H%M%S')
+        os.makedirs("./tmp/" + self.directory_name)
+
+    def __del__(self):
+        # delete empty directory
+        file_list = StoreFile.get_all_file_list("./tmp/" + self.directory_name)
+        if len(file_list) == 0:
+            os.rmdir("./tmp/" + self.directory_name)
 
     def get_all(self):
         files = self.sql_handler.get_all_files()
@@ -17,6 +30,7 @@ class Retriever:
             return
         sql = self.sql_handler.get_sql_contents(sql_file)
         print("SQL: " + sql)
-        query_result = self.db.get_from_sql(sql)
+        header, query_result = self.db.get_from_sql(sql)
         print("Result: " + str(query_result))
-
+        file_path = "./tmp/" + self.directory_name + "/" + sql_file[:-4]
+        StoreExcel.create_sheet_with_result(file_path, header, query_result)
--- store/store_excel.py
+++ store/store_excel.py
@@ -2,9 +2,28 @@ import openpyxl
 
 
 class StoreExcel:
+    def __init__(self, config=None):
+        self.tmp_path = "./tmp"
+        if config and "tmp_path" in config.keys():
+            self.tmp_path = config["tmp_path"]
+
     @staticmethod
     def create_sheet(name):
         wb = openpyxl.Workbook()
-        if not name.endsWith(".xlsx"):
+        if not name.endswith(".xlsx"):
             name += ".xlsx"
-        wb.save('tmp/' + name)
+        wb.save(name)
+
+    @staticmethod
+    def create_sheet_with_result(name, header, result):
+        wb = openpyxl.Workbook()
+        sheet_names = wb.get_sheet_names()
+        ws = wb.get_sheet_by_name(sheet_names[0])
+        if not name.endswith(".xlsx"):
+            name += ".xlsx"
+        for i in range(len(header)):
+            ws.cell(column=i+1, row=1).value = header[i][0]
+        for i in range(len(result)):
+            for j in range(len(result[0])):
+                ws.cell(column=j+1, row=i+2).value = result[i][j]
+        wb.save(name)
