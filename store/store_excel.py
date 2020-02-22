07e225251369de6ce7fd0073a28954df6ba29a4a
Taurin190 <taura.koichi@gmail.com>
2020-02-23 07:12:42 +0900

    add sanitize function and use it before put data

--- store/store_excel.py
+++ store/store_excel.py
@@ -1,4 +1,5 @@
 import openpyxl
+import re
 
 
 class StoreExcel:
@@ -25,5 +26,14 @@ class StoreExcel:
             ws.cell(column=i+1, row=1).value = header[i][0]
         for i in range(len(result)):
             for j in range(len(result[0])):
-                ws.cell(column=j+1, row=i+2).value = result[i][j]
+                ws.cell(column=j+1, row=i+2).value = StoreExcel.illegal_char_remover(result[i][j])
         wb.save(name)
+
+    @staticmethod
+    def illegal_char_remover(data):
+        ILLEGAL_CHARACTERS_RE = re.compile(
+            r'[\000-\010]|[\013-\014]|[\016-\037]|[\x00-\x1f\x7f-\x9f]|[\uffff]')
+        """Remove ILLEGAL CHARACTER."""
+        if not data:
+            return data
+        return ILLEGAL_CHARACTERS_RE.sub("", str(data))
